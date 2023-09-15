c4fa7295dbae8142a7bc77c43a60da426a727d8a
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-15 23:51:43 +0530

    Updated file: splitting components into small manageable components

--- my-first-app/src/components/ExpenseItem.js
+++ my-first-app/src/components/ExpenseItem.js
@@ -1,22 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ title, amount, date }) {
-  const month = date.toLocaleDateString("en-US", { month: "long" });
-  const year = date.toLocaleDateString("en-US", { year: "numeric" });
-  const day = date.toLocaleDateString("en-US", { day: "2-digit" });
-  const expenseTitle = title;
-  const expenseAmount = amount;
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
