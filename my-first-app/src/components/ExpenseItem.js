68f01fb7e9975901e8a5b4fae5c53ab01c75b272
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-15 22:02:47 +0530

    Updated file and added more complex JSX

--- my-first-app/src/components/ExpenseItem.js
+++ my-first-app/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem(){
-    return <h2>Expense item!</h2>
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <div>Car Insurance</div>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
