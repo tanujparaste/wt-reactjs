8adf8f1829b2a6d1e1b95bd626b862c7bda6ef1a
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-15 23:41:05 +0530

    Updated file: using standard JavaScript

--- my-first-app/src/components/ExpenseItem.js
+++ my-first-app/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem({ title, amount, date }) {
-  const expenseDate = date;
+  const month = date.toLocaleDateString("en-US", { month: "long" });
+  const year = date.toLocaleDateString("en-US", { year: "numeric" });
+  const day = date.toLocaleDateString("en-US", { day: "2-digit" });
   const expenseTitle = title;
   const expenseAmount = amount;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
